05adcc9de9064662a9a900dbb4e83adf65b01ad0
brilyanilhamsadewo <brilyan.ilham9i.36@gmail.com>
2021-05-26 15:38:09 +0700

    Role & Permission Users

--- resources/views/layouts/module/sidebar.blade.php
+++ resources/views/layouts/module/sidebar.blade.php
@@ -5,10 +5,8 @@
             style="opacity: .8">
         <span class="brand-text font-weight-light">POS</span>
     </a>
-​
-    <!-- Sidebar -->
+
     <div class="sidebar">
-        <!-- Sidebar user panel (optional) -->
         <div class="user-panel mt-3 pb-3 mb-3 d-flex">
             <div class="image">
                 <img src="{{ asset('dist/img/user2-160x160.jpg') }}" class="img-circle elevation-2" alt="User Image">
@@ -17,7 +15,7 @@
                 <a href="#" class="d-block">Daengweb.id</a>
             </div>
         </div>
-​
+
         <nav class="mt-2">
             <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
                 <li class="nav-item has-treeview menu-open">
@@ -29,6 +27,8 @@
                         </p>
                     </a>
                 </li>
+
+                @can('show products|delete products|create products')
                 <li class="nav-item has-treeview">
                     <a href="#" class="nav-link">
                         <i class="nav-icon fa fa-server"></i>
@@ -52,6 +52,39 @@
                         </li>
                     </ul>
                 </li>
+                @endcan
+                
+                {{-- @role('admin') --}}
+                <li class="nav-item has-treeview">
+                    <a href="#" class="nav-link">
+                        <i class="nav-icon fa fa-users"></i>
+                        <p>
+                            Manajemen Users
+                            <i class="right fa fa-angle-left"></i>
+                        </p>
+                    </a>
+                    <ul class="nav nav-treeview">
+                        <li class="nav-item">
+                            <a href="{{ route('role.index') }}" class="nav-link">
+                                <i class="fa fa-circle-o nav-icon"></i>
+                                <p>Role</p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="{{ route('users.roles_permission') }}" class="nav-link">
+                                <i class="fa fa-circle-o nav-icon"></i>
+                                <p>Role Permission</p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="{{ route('users.index') }}" class="nav-link">
+                                <i class="fa fa-circle-o nav-icon"></i>
+                                <p>Users</p>
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+                {{-- @endrole --}}
                 <li class="nav-item has-treeview">
                     <a class="nav-link" href="{{ route('logout') }}"
                         onclick="event.preventDefault();
@@ -61,7 +94,7 @@
                             {{ __('Logout') }}
                         </p>
                     </a>
-                ​
+
                     <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                         @csrf
                     </form>
